84a8d41cd388fda86dd299463e78887a8c0b36ed
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-21 21:59:35 +0200

    Update BookList component's name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Books from './components/Books';
+import BookList from './components/BookList';
 import Categories from './components/Categories';
 import Header from './components/Header';
 
@@ -10,7 +10,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Books />} />
+        <Route path="/" element={<BookList />} />
         <Route path="/Categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
