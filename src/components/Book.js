6a543610650decfe51dfae8214831ac9cc069b7e
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-22 21:43:46 +0200

    Add UI elements to Book, BookList and CreateBook components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { remove } from '../redux/books/books';
 
-const Book = ({ category, title, author }) => (
-  <li>
-    <h3>{category}</h3>
-    <h2>{title}</h2>
-    <h3>{author}</h3>
-    <button type="button">Remove</button>
-  </li>
-);
+const Book = ({
+  title, author, id, category,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <li id={id}>
+      <h3>{category}</h3>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <button type="button" onClick={(e) => { dispatch(remove(e.target.parentNode.id)); }}>Remove</button>
+    </li>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
 };
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,22 @@
 import '../assets/BookList.css';
-import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import CreateBook from './CreateBook';
 
 const Books = () => {
-  const store = [
-    {
-      category: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      category: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      category: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-    },
-  ];
+  const books = useSelector((state) => state.booksReducer);
+
   return (
     <div className="book-list">
       <ul className="book">
-        {store.map((book) => (
-          <Book category={book.category} title={book.title} author={book.author} key={uuidv4()} />
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            category={book.category}
+          />
         ))}
       </ul>
       <hr />
